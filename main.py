828ec6d61a9de8f1dd0632f4d36d2751f121c858
stoneshik <strebilly@mail.ru>
2023-06-01 19:17:27 +0300

    second

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ class Equation:
     """
     Класс обертка для уравнений
     """
-    def __init__(self, equation_func, symbol: Symbol) -> None:
+    def __init__(self, equation_func, first_symbol: Symbol, second_symbol: Symbol) -> None:
         self.equation_func = equation_func
-        self.symbol = symbol
+        self.first_symbol = first_symbol
+        self.second_symbol = second_symbol
 
     def get_string(self) -> str:
         return latex(self.equation_func)
@@ -31,6 +32,7 @@ class SolutionMethod(ABC):
                  x_n: float,
                  h: float,
                  epsilon: float,
+                 p: int,
                  header_table) -> None:
         assert x_zero != x_n, "Значения x0 и xn должны быть различны"
         assert x_zero < x_n, "Значение x0 должно быть меньше xn"
@@ -43,13 +45,14 @@ class SolutionMethod(ABC):
         self._x_n = x_n
         self._h = h
         self._epsilon = epsilon
+        self._p = p
         self._header_table = header_table
+        self._solution = []
 
     @abstractmethod
     def calc(self) -> PrettyTable:
         pass
 
-    @abstractmethod
     def draw(self) -> None:
         pass
 
@@ -60,29 +63,56 @@ class EulerMethod(SolutionMethod):
     """
     name: str = 'метод Эйлера'
 
-    def __init__(self, equation: Equation, a: float, b: float, n: int, epsilon: float = 0.01) -> None:
-        super().__init__(equation, a, b, n, 4, epsilon)
+    def __init__(self,
+                 equation_diff: Equation,
+                 equation_solution: Equation,
+                 y_zero: float,
+                 x_zero: float,
+                 x_n: float,
+                 h: float,
+                 epsilon: float = 0.01) -> None:
+        super().__init__(
+            equation_diff, equation_solution, y_zero, x_zero, x_n, h, epsilon, 1,
+            ['i', 'xi', 'yi^h', 'yi^{h/2}', 'f(xi, yi)^h', 'f(xi, yi)^{h/2}', 'R', 'Точное решение']
+        )
 
     def calc(self) -> tuple[float, int]:
-        func = self._equation.equation_func
-        x = self._equation.symbol
-        integral_value_first: float = 0.0
-        n: int = self._n
+        func = self._equation_diff.equation_func
+        func_solution = self._equation_solution.equation_func
+        x = self._equation_diff.first_symbol
+        y = self._equation_diff.second_symbol
+        table: PrettyTable = PrettyTable()
+        table.field_names = self._header_table
+        table.add_row([
+            0,
+            self._x_zero,
+            self._y_zero,
+            self._y_zero,
+            func.subs({x: self._x_zero, y: self._y_zero}),
+            func.subs({x: self._x_zero, y: self._y_zero}),
+            0,
+            func_solution.subs({x: self._x_zero, y: self._y_zero})])
+        rows: list = []
+        h: float = self._h
+        x_i: float = self._x_zero
+        y_i: float = self._y_zero
+        y_i_h_divide_2: float = self._y_zero
         while True:
-            integral_value_zero = integral_value_first
-            integral_value_first = 0.0
-            h = (self._b - self._a) / n
-            for i in range(1, n):
-                x_i = self._a + h * i
-                if i % 2 == 0:
-                    integral_value_first += 2 * func.subs(x, x_i).evalf()
-                    continue
-                integral_value_first += 4 * func.subs(x, x_i).evalf()
-            integral_value_first += func.subs(x, self._a).evalf() + func.subs(x, self._b).evalf()
-            integral_value_first *= h / 3
-            if abs((integral_value_first - integral_value_zero) / (2 ** self._k - 1)) < self._epsilon:
-                break
-            n *= 2
+            y_i_plus_1: float = x_i + h * func.subs({x: x_i, y: y_i})
+            y_i_h_divide_2_plus_1: float = \
+                x_i + h/2 + (h/2) * func.subs({x: x_i + h/2, y: x_i + h/2 * func.subs({x: x_i, y: y_i_h_divide_2})})
+            r: float = abs(y_i_plus_1 - y_i_h_divide_2_plus_1) / (2 ** self._p - 1)
+            if r > self._epsilon:
+                rows = []
+                h /= 4
+                x_i = self._x_zero
+                y_i = self._y_zero
+                y_i_h_divide_2 = self._y_zero
+                continue
+            x_i += h
+            y_i = y_i_plus_1
+            y_i_h_divide_2 = y_i_h_divide_2_plus_1
+        table.add_rows(rows)
         return integral_value_first, n
 
 
@@ -153,14 +183,14 @@ def main():
         Equation(x ** 5 - 1.18, x)
     )
     solution_methods = (
-        SimpsonMethod,
+        EulerMethod,
     )
     solution_method = input_data(equations, solution_methods)
     if solution_method is None:
         return
-    integral_value, n = solution_method.calc()
-    print(f"Вычисленное значение интеграла {integral_value}")
-    print(f"Число разбиения интервала интегрирования для достижения требуемой точности {n}")
+    table: PrettyTable = solution_method.calc()
+    print(table)
+    solution_method.draw()
 
 
 if __name__ == '__main__':
